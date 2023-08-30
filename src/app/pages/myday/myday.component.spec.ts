b25413645d5b429894af9b1a33c36c68322a9522
Rajaraman <paradox2cr@gmail.com>
2023-08-30 13:25:27 +0530

    Intial

---
+++ src/app/pages/myday/myday.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MydayComponent } from './myday.component';
+
+describe('MydayComponent', () => {
+  let component: MydayComponent;
+  let fixture: ComponentFixture<MydayComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MydayComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MydayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
